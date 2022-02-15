c53899ff9556be53277cf20302fd81aae3e9a0ae
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-02-15 01:28:10 +0100

    [✨] implementar App

---
+++ src/main/java/App.java
@@ -0,0 +1,36 @@
+import edu.poniperro.mollapp.administrador.ProgramadorTasques;
+import edu.poniperro.mollapp.clients.Mollapp;
+import edu.poniperro.mollapp.filtres.Autoritzacio;
+import edu.poniperro.mollapp.targets.Vehicle;
+import edu.poniperro.mollapp.filtres.Autenticacio;
+
+public class App {
+    public static void main(String[] args) {
+
+        /**
+         * Configuració del programador de tasques
+         * del sistema amb el tipus de target triat:
+         * des de vehicles a qualsevol cosa que admiteix
+         * la recepció d'un missatge.
+         */
+        ProgramadorTasques programadorTasques = new ProgramadorTasques(new Vehicle());
+
+        /**
+         * Afegir al sistema les tasques que volem que el sistema
+         * executi al rebre la petició del client.
+         */
+        programadorTasques.setTasca(new Autenticacio());
+        programadorTasques.setTasca(new Autoritzacio());
+
+        /**
+         * Configuració de l'app client per a que
+         * executi les tasques programades i
+         * enviï el misstage al sistema.
+         */
+        Mollapp mollapp = new Mollapp();
+        mollapp.setProgramadorTasques(programadorTasques);
+        mollapp.enviarPeticio("Francesc");
+    }
+}
+
+
